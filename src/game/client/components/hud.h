ddf310ef7df08eb3ebcdcc6e3a0bba61dec480ce
Alexander Akulich <akulichalexander@gmail.com>
2022-01-02 00:43:11 +0300

    Draft hook protection icon

--- src/game/client/components/hud.h
+++ src/game/client/components/hud.h
@@ -70,7 +70,9 @@ class CHud : public CComponent
 	void RenderTeambalanceWarning();
 
 	void PrepareAmmoHealthAndArmorQuads();
+	void PrepareInfclassHudQuads();
 	void RenderAmmoHealthAndArmor(const CNetObj_Character *pCharacter);
+	void RenderStatusIcons(int ClientID);
 
 	void PreparePlayerStateQuads();
 	void RenderPlayerState(int ClientId);
@@ -168,6 +170,8 @@ private:
 	int m_PracticeModeOffset;
 	int m_Team0ModeOffset;
 	int m_LockModeOffset;
+
+	int m_IcStatusIconOffset;
 };
 
 #endif

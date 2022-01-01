a0d8d96a0b7554b20a31202874a88dced15fd7b0
Alexander Akulich <akulichalexander@gmail.com>
2022-01-02 02:44:21 +0300

    Add owner icon to own objects

--- datasrc/network.py
+++ datasrc/network.py
@@ -49,6 +49,7 @@ DraggerTypes = ["WEAK", "WEAK_NW", "NORMAL", "NORMAL_NW", "STRONG", "STRONG_NW"]
 GunTypes = ["UNFREEZE", "EXPLOSIVE", "FREEZE", "EXPFREEZE"]
 
 InfClassPlayerFlags = ["INFECTED", "HOOK_PROTECTION_OFF"]
+InfClassObjectFlags = ["HAS_SECOND_POSITION"]
 
 Emoticons = ["OOP", "EXCLAMATION", "HEARTS", "DROP", "DOTDOT", "MUSIC", "SORRY", "GHOST", "SUSHI", "SPLATTEE", "DEVILTEE", "ZOMG", "ZZZ", "WTF", "EYES", "QUESTION"]
 
@@ -106,6 +107,7 @@ Flags = [
 	Flags("LASERFLAG", LaserFlags),
 	Flags("PICKUPFLAG", PickupFlags),
 	Flags("INFCLASS_PLAYER_FLAG", InfClassPlayerFlags),
+	Flags("INFCLASS_OBJECT_FLAG", InfClassObjectFlags),
 ]
 
 Objects = [
@@ -250,6 +252,15 @@ Objects = [
 
 	NetObjectEx("MyOwnObject", "my-own-object@heinrich5991.de", [
 		NetIntAny("m_Test"),
+	]),
+
+	NetObjectEx("InfClassObject", "object@infclass", [
+		NetIntAny("m_Flags"),
+		NetIntRange("m_Owner", -1, 'MAX_CLIENTS-1'),
+		NetIntAny("m_X"),
+		NetIntAny("m_Y"),
+		NetIntAny("m_X2"),
+		NetIntAny("m_Y2"),
 	]),
 
 	NetObjectEx("InfClassPlayer", "player@infclass", [

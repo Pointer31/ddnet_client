e0ab7d9d05aa38109f0dbfd758869649d5962b0d
Alexander Akulich <akulichalexander@gmail.com>
2022-02-01 23:41:43 +0300

    Add custom infclass HUD for Hero class

--- datasrc/network.py
+++ datasrc/network.py
@@ -262,6 +262,19 @@ Objects = [
 		NetIntRange("m_Class", -1, 255),
 	]),
 
+	NetObjectEx("InfClassGameInfo", "gameinfo@infclass", [
+		NetIntAny("m_Version", default=0),
+		NetIntAny("m_Flags", default=0),
+		NetIntRange("m_TimeLimitInSeconds", 0, 'max_int', default=0),
+		NetIntAny("m_HeroGiftTick", default=-1),
+	], validate_size=False),
+
+	NetObjectEx("InfClassClassInfo", "classinfo@infclass", [
+		NetIntAny("m_Flags"),
+		NetIntRange("m_Class", -1, 255),
+		NetIntAny("m_Data1"),
+	]),
+
 	NetObjectEx("DDNetCharacter", "character@netobj.ddnet.tw", [
 		NetIntAny("m_Flags", default=0),
 		NetTick("m_FreezeEnd", default=0),

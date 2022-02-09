db07d0a52a4f5dddc1c5b3b52fe6d5038e1ecdf6
Alexander Akulich <akulichalexander@gmail.com>
2022-02-09 20:12:14 +0300

    Draft Infclass controls (report location) with settings page

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -154,6 +154,7 @@ void CGameClient::OnConsoleInit()
 					      &m_Emoticon,
 					      &m_InfoMessages,
 					      &m_Chat,
+					      &m_InfCCommands,
 					      &m_Broadcast,
 					      &m_DebugHud,
 					      &m_TouchControls,

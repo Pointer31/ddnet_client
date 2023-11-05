a3e9005234316581ccba6e668ec31deac86d8128
Alexander Akulich <akulichalexander@gmail.com>
2023-11-05 07:10:14 +0300

    Implement client-side Soldier Bomb rendering

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -681,6 +681,7 @@ void CGameClient::OnReset()
 	m_InfClassHeroGiftTick = -1;
 	m_InfclassGameInfoVersion = 0;
 	m_InfClassWhiteHoleMinKills = 0;
+	m_InfClassSoldierBombs = 0;
 
 	m_Teams.Reset();
 	m_GameWorld.Clear();
@@ -1063,6 +1064,7 @@ void CGameClient::OnMessage(int MsgId, CUnpacker *pUnpacker, int Conn, bool Dumm
 
 		m_InfclassGameParamsVersion = pMsg->m_Version;
 		m_InfClassWhiteHoleMinKills = pMsg->m_WhiteHoleMinKills;
+		m_InfClassSoldierBombs = pMsg->m_SoldierBombs;
 		return;
 	}
 

a246db64656ae08ee6f2daccc0d640da2f3b3b78
Alexander Akulich <akulichalexander@gmail.com>
2024-12-01 10:44:18 +0300

    ConfigManager: Implement Load() which takes care of all domains

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -4600,13 +4600,6 @@ static bool UnknownArgumentCallback(const char *pCommand, void *pUser)
 	return false;
 }
 
-static bool SaveUnknownCommandCallback(const char *pCommand, void *pUser)
-{
-	CClient *pClient = static_cast<CClient *>(pUser);
-	pClient->ConfigManager()->StoreUnknownCommand(pCommand);
-	return true;
-}
-
 /*
 	Server Time
 	Client Mirror Time
@@ -4902,19 +4895,17 @@ int main(int argc, const char **argv)
 	// init client's interfaces
 	pClient->InitInterfaces();
 
-	// execute config file
-	if(pStorage->FileExists(CONFIG_FILE, IStorage::TYPE_ALL))
+	std::vector<const char *> vFailedConfigFiles;
+	if(!pConfigManager->Load(&vFailedConfigFiles))
 	{
-		pConsole->SetUnknownCommandCallback(SaveUnknownCommandCallback, pClient);
-		if(!pConsole->ExecuteFile(CONFIG_FILE, IConsole::CLIENT_ID_UNSPECIFIED))
+		char aError[128];
+		for(const char *pConfigFileName : vFailedConfigFiles)
 		{
-			const char *pError = "Failed to load config from '" CONFIG_FILE "'.";
-			log_error("client", "%s", pError);
-			pClient->ShowMessageBox({.m_pTitle = "Config File Error", .m_pMessage = pError});
-			PerformAllCleanup();
-			return -1;
+			str_format(aError, sizeof(aError), "Failed to load config from '%s'.", pConfigFileName);
+			pClient->ShowMessageBox({.m_pTitle = "Config File Error", .m_pMessage = aError});
 		}
-		pConsole->SetUnknownCommandCallback(IConsole::EmptyUnknownCommandCallback, nullptr);
+		PerformAllCleanup();
+		return -1;
 	}
 
 	// execute autoexec file

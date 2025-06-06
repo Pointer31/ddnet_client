7b3e65d85b33ed1ecde0c939e63c02ca6c5539f1
KebsCS <git@kebs.dev>
2025-06-06 18:08:09 +0200

    cherry pick rotating weapons

--- datasrc/network.py
+++ datasrc/network.py
@@ -40,6 +40,10 @@ LaserFlags = [
 	"NO_PREDICT",
 ]
 
+PickupFlags = [
+	"XFLIP", "YFLIP", "ROTATE",
+]
+
 LaserTypes = ["RIFLE", "SHOTGUN", "DOOR", "FREEZE", "DRAGGER", "GUN", "PLASMA"]
 DraggerTypes = ["WEAK", "WEAK_NW", "NORMAL", "NORMAL_NW", "STRONG", "STRONG_NW"]
 GunTypes = ["UNFREEZE", "EXPLOSIVE", "FREEZE", "EXPFREEZE"]
@@ -107,6 +111,7 @@ Flags = [
 	Flags("INFCLASS_PLAYER_FLAG", InfClassPlayerFlags),
 	Flags("INFCLASS_OBJECT_FLAG", InfClassObjectFlags),
 	Flags("INFCLASS_CLASSINFO_FLAG", InfClassClassInfoFlags),
+	Flags("PICKUPFLAG", PickupFlags),
 ]
 
 Objects = [
@@ -357,6 +362,7 @@ Objects = [
 		NetIntRange("m_Type", 0, 'max_int'),
 		NetIntRange("m_Subtype", 0, 'max_int'),
 		NetIntAny("m_SwitchNumber"),
+		NetIntAny("m_Flags", default=0),
 	]),
 
 	NetObjectEx("DDNetSpectatorInfo", "spectator-info@netobj.ddnet.org", [

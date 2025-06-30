fc93fb04b54787bef9237e1a00409eb81a552e06
Pointer31 <pointer.31.31.31@gmail.com>
2025-06-30 15:38:08 +0200

    add cl_extra_particles and cl_blood_particles

--- src/game/client/components/effects.h
+++ src/game/client/components/effects.h
@@ -33,6 +33,7 @@ public:
 	void Explosion(vec2 Pos, float Alpha);
 	void HammerHit(vec2 Pos, float Alpha);
 	void AirJump(vec2 Pos, float Alpha);
+	void LaserBounce(vec2 Pos, float Alpha = 1.0f);
 	void DamageIndicator(vec2 Pos, vec2 Dir, float Alpha);
 	void PlayerSpawn(vec2 Pos, float Alpha);
 	void PlayerDeath(vec2 Pos, int ClientId, float Alpha);

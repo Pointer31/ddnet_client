4ac36b6c61b6d7b5c06d5b88e654a8139d5985f0
Pointer31 <pointer.31.31.31@gmail.com>
2025-07-24 15:06:06 +0200

    extra particles shows correct color for shotgun

--- src/game/client/components/effects.h
+++ src/game/client/components/effects.h
@@ -33,7 +33,7 @@ public:
 	void Explosion(vec2 Pos, float Alpha);
 	void HammerHit(vec2 Pos, float Alpha);
 	void AirJump(vec2 Pos, float Alpha);
-	void LaserBounce(vec2 Pos, float Alpha = 1.0f);
+	void LaserBounce(vec2 Pos, float Alpha = 1.0f, unsigned int Color = g_Config.m_ClLaserRifleInnerColor);
 	void DamageIndicator(vec2 Pos, vec2 Dir, float Alpha);
 	void PlayerSpawn(vec2 Pos, float Alpha);
 	void PlayerDeath(vec2 Pos, int ClientId, float Alpha);

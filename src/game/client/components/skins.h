3f0b9a6d48e897d2dd823a2c25037e82b0114bd4
Robert Müller <robytemueller@gmail.com>
2025-08-10 16:55:44 +0200

    Fix skin prefix setting not working correctly anymore
    Adapt `CSkins::FindContainerOrNullptr` function to consider skin prefix instead of only considering it in the `FindOrNullptr` function.
    
    Refresh skins when changing skin prefix config variable.
    
    When loading a skin whose name begins with the configured skin prefix, also correctly reload skins that have the unprefixed name.
    
    Closes #10655.

--- src/game/client/components/skins.h
+++ src/game/client/components/skins.h
@@ -248,7 +248,7 @@ public:
 	CSkinList &SkinList();
 
 	const CSkinContainer *FindContainerOrNullptr(const char *pName);
-	const CSkin *FindOrNullptr(const char *pName, bool IgnorePrefix = false);
+	const CSkin *FindOrNullptr(const char *pName);
 	const CSkin *Find(const char *pName);
 
 	void AddFavorite(const char *pName);
@@ -259,6 +259,8 @@ public:
 
 	static bool IsVanillaSkin(const char *pName);
 	static bool IsInfclassSkin(const char *pName);
+	const char *SkinPrefix() const;
+
 	static bool IsSpecialSkin(const char *pName);
 
 private:
@@ -311,7 +313,7 @@ private:
 	bool LoadSkinData(const char *pName, CSkinLoadData &Data) const;
 	void LoadSkinFinish(CSkinContainer *pSkinContainer, const CSkinLoadData &Data);
 	void LoadSkinDirect(const char *pName);
-	const CSkin *FindImpl(const char *pName);
+	const CSkinContainer *FindContainerImpl(const char *pName);
 	static int SkinScan(const char *pName, int IsDir, int StorageType, void *pUser);
 
 	void UpdateUnloadSkins(CSkinLoadingStats &Stats);

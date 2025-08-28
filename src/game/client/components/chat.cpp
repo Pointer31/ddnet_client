03ea85f61ef6d5166c26181c99b0570b7345c4e8
Pointer31 <pointer.31.31.31@gmail.com>
2025-08-29 00:18:13 +0200

    add cl_chat_transparency to control transparency of the chat background

--- src/game/client/components/chat.cpp
+++ src/game/client/components/chat.cpp
@@ -1266,7 +1266,7 @@ void CChat::OnRender()
 			Graphics()->TextureClear();
 			if(Line.m_QuadContainerIndex != -1)
 			{
-				Graphics()->SetColor(0, 0, 0, 0.12f * Blend);
+				Graphics()->SetColor(0, 0, 0, g_Config.m_ClChatTransparency / 1000.0f * Blend);
 				Graphics()->RenderQuadContainerEx(Line.m_QuadContainerIndex, 0, -1, 0, ((y + RealMsgPaddingY / 2.0f) - Line.m_TextYOffset));
 			}
 		}

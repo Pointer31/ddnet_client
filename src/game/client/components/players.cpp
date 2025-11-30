infclass/infclass-client

Show infected hook for infected classes

src/game/client/components/players.cpp
@@ -127,7 +127,15 @@ void CPlayers::RenderHook(
 		float d = distance(Pos, HookPos);
 		vec2 Dir = normalize(Pos - HookPos);
 
-		Graphics()->TextureSet(GameClient()->m_GameSkin.m_SpriteHookHead);
+		bool InfectedHook = pRenderInfo->m_InfectedHook;
+		if(InfectedHook)
+		{
+			Graphics()->TextureSet(GameClient()->m_InfclassSkin.m_SpriteHookHead);
+		}
+		else
+		{
+			Graphics()->TextureSet(GameClient()->m_GameSkin.m_SpriteHookHead);
+		}
 		Graphics()->QuadsSetRotation(angle(Dir) + pi);
 		// render head
 		int QuadOffset = NUM_WEAPONS * 2 + 2;
@@ -148,7 +156,14 @@ void CPlayers::RenderHook(
 			s_HookChainRenderInfo[HookChainCount].m_Scale = 1;
 			s_HookChainRenderInfo[HookChainCount].m_Rotation = angle(Dir) + pi;
 		}
-		Graphics()->TextureSet(GameClient()->m_GameSkin.m_SpriteHookChain);
+		if(InfectedHook)
+		{
+			Graphics()->TextureSet(GameClient()->m_InfclassSkin.m_SpriteHookChain);
+		}
+		else
+		{
+			Graphics()->TextureSet(GameClient()->m_GameSkin.m_SpriteHookChain);
+		}
 		Graphics()->RenderQuadContainerAsSpriteMultiple(m_WeaponEmoteQuadContainerIndex, QuadOffset, HookChainCount, s_HookChainRenderInfo);
 
 		Graphics()->QuadsSetRotation(0);
@@ -648,7 +663,9 @@ void CPlayers::OnRender()
 		IsTeamplay = (m_pClient->m_Snap.m_pGameInfoObj->m_GameFlags & GAMEFLAG_TEAMS) != 0;
 	for(int i = 0; i < MAX_CLIENTS; ++i)
 	{
+		const CGameClient::CClientData *pClientData = &m_pClient->m_aClients[i];
 		m_aRenderInfo[i] = m_pClient->m_aClients[i].m_RenderInfo;
+		m_aRenderInfo[i].m_InfectedHook = pClientData->m_InfClassPlayerFlags & INFCLASS_PLAYER_FLAG_INFECTED;
 		if(m_pClient->m_Snap.m_aCharacters[i].m_Cur.m_Weapon == WEAPON_NINJA && g_Config.m_ClShowNinja)
 		{
 			// change the skin for the player to the ninja
@@ -675,6 +692,7 @@ void CPlayers::OnRender()
 	m_RenderInfoSpec.m_ColorableRenderSkin = pSkin->m_ColorableSkin;
 	m_RenderInfoSpec.m_BloodColor = pSkin->m_BloodColor;
 	m_RenderInfoSpec.m_SkinMetrics = pSkin->m_Metrics;
+	m_RenderInfoSpec.m_InfectedHook = false;
 	m_RenderInfoSpec.m_CustomColoredSkin = false;
 	m_RenderInfoSpec.m_Size = 64.0f;
 

Pointer31/ddnet_client

src/game/client/components/players.cpp
@@ -392,7 +392,15 @@ void CPlayers::RenderHook(
 		float d = distance(Pos, HookPos);
 		vec2 Dir = normalize(Pos - HookPos);
 
-		Graphics()->TextureSet(GameClient()->m_GameSkin.m_SpriteHookHead);
+		bool InfectedHook = pRenderInfo->m_InfectedHook;
+		if(InfectedHook)
+		{
+			Graphics()->TextureSet(GameClient()->m_InfclassSkin.m_SpriteHookHead);
+		}
+		else
+		{
+			Graphics()->TextureSet(GameClient()->m_GameSkin.m_SpriteHookHead);
+		}
 		Graphics()->QuadsSetRotation(angle(Dir) + pi);
 		// render head
 		int QuadOffset = NUM_WEAPONS * 2 + 2;
@@ -411,7 +419,14 @@ void CPlayers::RenderHook(
 			s_aHookChainRenderInfo[HookChainCount].m_Scale = 1;
 			s_aHookChainRenderInfo[HookChainCount].m_Rotation = angle(Dir) + pi;
 		}
-		Graphics()->TextureSet(GameClient()->m_GameSkin.m_SpriteHookChain);
+		if(InfectedHook)
+		{
+			Graphics()->TextureSet(GameClient()->m_InfclassSkin.m_SpriteHookChain);
+		}
+		else
+		{
+			Graphics()->TextureSet(GameClient()->m_GameSkin.m_SpriteHookChain);
+		}
 		Graphics()->RenderQuadContainerAsSpriteMultiple(m_WeaponEmoteQuadContainerIndex, QuadOffset, HookChainCount, s_aHookChainRenderInfo);
 
 		Graphics()->QuadsSetRotation(0);
@@ -831,6 +846,9 @@ void CPlayers::OnRender()
 			aRenderInfo[i].m_TeeRenderFlags |= TEE_EFFECT_FROZEN;
 		if(m_pClient->m_aClients[i].m_Predicted.m_Invincible)
 			aRenderInfo[i].m_TeeRenderFlags |= TEE_EFFECT_SPARKLE;
+
+		const CGameClient::CClientData *pClientData = &m_pClient->m_aClients[i];
+		aRenderInfo[i].m_InfectedHook = pClientData->m_InfClassPlayerFlags & INFCLASS_PLAYER_FLAG_INFECTED;
 
 		const bool Frozen = m_pClient->m_aClients[i].m_Predicted.m_FreezeEnd != 0;
 		if((m_pClient->m_aClients[i].m_RenderCur.m_Weapon == WEAPON_NINJA || (Frozen && !m_pClient->m_GameInfo.m_NoSkinChangeForFrozen)) && g_Config.m_ClShowNinja)
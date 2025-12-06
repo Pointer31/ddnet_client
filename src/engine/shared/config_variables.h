997ff88550a360f9ed7411b562132f660fd2f455
Pointer31 <pointer.31.31.31@gmail.com>
2025-12-06 14:16:43 +0100

    improve cl_weather_snow (no longer (heavily) fps dependent)

--- src/engine/shared/config_variables.h
+++ src/engine/shared/config_variables.h
@@ -744,7 +744,7 @@ MACRO_CONFIG_INT(ClChatInputBackground, cl_chat_input_background, 1, 0, 1, CFGFL
 MACRO_CONFIG_INT(ClReplaceEscapeKey, cl_replace_escape_key, 0, 0, 512, CFGFLAG_SAVE | CFGFLAG_CLIENT, "Which key (as integer) to map to KEY_ESC. 0 to disable")
 MACRO_CONFIG_INT(ClParticleLimit, cl_particle_limit, 8192, 0, 8192, CFGFLAG_SAVE | CFGFLAG_CLIENT, "What the limit on the amount of particles should be")
 
-MACRO_CONFIG_INT(ClWeatherSnow, cl_weather_snow, 0, 0, 1, CFGFLAG_CLIENT, "Whether to spawn snow")
+MACRO_CONFIG_INT(ClWeatherSnow, cl_weather_snow, 0, 0, 15, CFGFLAG_CLIENT, "Whether to spawn snow")
 MACRO_CONFIG_INT(ClUIWidth, cl_ui_width, 600, 400, 1000, CFGFLAG_SAVE | CFGFLAG_CLIENT, "Size of UI (low values may break some UI, might need restart)")
 //
 MACRO_CONFIG_INT(ClHammerRotatesWithCursor, tc_hammer_rotates_with_cursor, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Allow your hammer to rotate like other weapons")
--- src/game/client/components/effects.cpp
+++ src/game/client/components/effects.cpp
@@ -485,8 +485,8 @@ void CEffects::HammerHit(vec2 Pos, float Alpha)
 
 void CEffects::Snow(vec2 Pos, float Alpha)
 {
-	if(!m_Add5hz)
-		return;
+	// if(!m_Add5hz)
+	// 	return;
 
 	float Size = 10;
 

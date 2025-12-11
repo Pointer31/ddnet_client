infclass/infclass-client

Use DDRace HUD for Infclass (if available)

src/game/client/components/hud.cpp
@@ -888,6 +888,7 @@ void CHud::PreparePlayerStateQuads()
 	}
 
 	// Quads for displaying capabilities
+	m_HookProtectionOffset = RenderTools()->QuadContainerAddSprite(m_HudQuadContainerIndex, 0.f, 0.f, 12.f, 12.f);
 	m_EndlessJumpOffset = RenderTools()->QuadContainerAddSprite(m_HudQuadContainerIndex, 0.f, 0.f, 12.f, 12.f);
 	m_EndlessHookOffset = RenderTools()->QuadContainerAddSprite(m_HudQuadContainerIndex, 0.f, 0.f, 12.f, 12.f);
 	m_JetpackOffset = RenderTools()->QuadContainerAddSprite(m_HudQuadContainerIndex, 0.f, 0.f, 12.f, 12.f);
@@ -919,6 +920,17 @@ void CHud::PreparePlayerStateQuads()
 
 void CHud::RenderPlayerState(const int ClientID)
 {
+	if(m_pClient->m_GameInfo.m_InfClass)
+	{
+		if(!g_Config.m_InfUseDDRaceHUD)
+			return;
+	}
+	else
+	{
+		if(!g_Config.m_ClShowhudDDRace)
+			return;
+	}
+
 	Graphics()->SetColor(1.f, 1.f, 1.f, 1.f);
 
 	// pCharacter contains the predicted character for local players or the last snap for players who are spectated
@@ -1042,6 +1054,17 @@ void CHud::RenderPlayerState(const int ClientID)
 		y += 12;
 	}
 	bool HasCapabilities = false;
+	
+	const CGameClient::CClientData *pClientData = &m_pClient->m_aClients[ClientID];
+	int InfclassPlayerFlags = pClientData->m_InfClassPlayerFlags;
+	
+	if(InfclassPlayerFlags & INFCLASS_PLAYER_FLAG_HOOK_PROTECTION_OFF)
+	{
+		HasCapabilities = true;
+		Graphics()->TextureSet(GameClient()->m_InfclassSkin.m_SpriteStatusHookable);
+		Graphics()->RenderQuadContainerAsSprite(m_HudQuadContainerIndex, m_HookProtectionOffset, x, y);
+		x += 12;
+	}
 	if(pCharacter->m_EndlessJump)
 	{
 		HasCapabilities = true;
@@ -1541,7 +1564,13 @@ void CHud::RenderStatusIcons(int ClientID)
 	{
 		return;
 	}
-
+	
+	if(g_Config.m_InfUseDDRaceHUD && GameClient()->m_GameInfo.m_HudDDRace)
+	{
+		// We render hook protection icon as a part of RenderPlayerState() if DDRace HUD is active
+		return;
+	}
+	
 	const CGameClient::CClientData *pClientData = &m_pClient->m_aClients[ClientID];
 	int InfclassPlayerFlags = pClientData->m_InfClassPlayerFlags;
 
@@ -1882,7 +1911,7 @@ void CHud::OnRender()
 				RenderStatusIcons(m_pClient->m_aLocalIDs[g_Config.m_ClDummy]);
 				RenderObjectOwnerIcons(m_pClient->m_aLocalIDs[g_Config.m_ClDummy]);
 			}
-			if(m_pClient->m_Snap.m_aCharacters[m_pClient->m_Snap.m_LocalClientID].m_HasExtendedData && g_Config.m_ClShowhudDDRace && GameClient()->m_GameInfo.m_HudDDRace)
+			if(m_pClient->m_Snap.m_aCharacters[m_pClient->m_Snap.m_LocalClientID].m_HasExtendedData && GameClient()->m_GameInfo.m_HudDDRace)
 			{
 				RenderPlayerState(m_pClient->m_Snap.m_LocalClientID);
 			}
@@ -1904,7 +1933,6 @@ void CHud::OnRender()
 			}
 			if(SpectatorID != SPEC_FREEVIEW &&
 				m_pClient->m_Snap.m_aCharacters[SpectatorID].m_HasExtendedData &&
-				g_Config.m_ClShowhudDDRace &&
 				(!GameClient()->m_MultiViewActivated || GameClient()->m_MultiViewShowHud) &&
 				GameClient()->m_GameInfo.m_HudDDRace)
 			{

src/game/client/components/menus_settings.cpp
@@ -2830,6 +2830,7 @@ void CMenus::RenderSettingsAppearance(CUIRect MainView)
 		RightView.HSplitTop(SectionTotalMargin + 4 * LineSize, &Section, &RightView);
 		Section.Margin(SectionMargin, &Section);
 
+		DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_InfUseDDRaceHUD, Localize("Use DDRace HUD for Infclass"), &g_Config.m_InfUseDDRaceHUD, &Section, LineSize);
 		DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_InfcShowHookProtection, Localize("Show player hook protection"), &g_Config.m_InfcShowHookProtection, &Section, LineSize);
 	}
 	else if(s_CurTab == APPEARANCE_TAB_CHAT)

Pointer31/ddnet_client

src/game/client/components/hud.cpp
@@ -917,6 +917,7 @@ void CHud::PreparePlayerStateQuads()
 	}
 
 	// Quads for displaying capabilities
+	m_HookProtectionOffset = Graphics()->QuadContainerAddSprite(m_HudQuadContainerIndex, 0.f, 0.f, 12.f, 12.f);
 	m_EndlessJumpOffset = Graphics()->QuadContainerAddSprite(m_HudQuadContainerIndex, 0.f, 0.f, 12.f, 12.f);
 	m_EndlessHookOffset = Graphics()->QuadContainerAddSprite(m_HudQuadContainerIndex, 0.f, 0.f, 12.f, 12.f);
 	m_JetpackOffset = Graphics()->QuadContainerAddSprite(m_HudQuadContainerIndex, 0.f, 0.f, 12.f, 12.f);
@@ -950,6 +951,17 @@ void CHud::PreparePlayerStateQuads()
 
 void CHud::RenderPlayerState(const int ClientId)
 {
+	if(GameClient()->m_GameInfo.m_InfClass)
+	{
+		if(!g_Config.m_InfUseDDRaceHUD)
+			return;
+	}
+	else
+	{
+		if(!g_Config.m_ClShowhudDDRace)
+			return;
+	}
+
 	Graphics()->SetColor(1.f, 1.f, 1.f, 1.f);
 
 	// pCharacter contains the predicted character for local players or the last snap for players who are spectated
@@ -1073,6 +1085,17 @@ void CHud::RenderPlayerState(const int ClientId)
 		y += 12;
 	}
 	bool HasCapabilities = false;
+
+	const CGameClient::CClientData *pClientData = &GameClient()->m_aClients[ClientId];
+	int InfclassPlayerFlags = pClientData->m_InfClassPlayerFlags;
+
+	if(InfclassPlayerFlags & INFCLASS_PLAYER_FLAG_HOOK_PROTECTION_OFF)
+	{
+		HasCapabilities = true;
+		Graphics()->TextureSet(GameClient()->m_InfclassSkin.m_SpriteStatusHookable);
+		Graphics()->RenderQuadContainerAsSprite(m_HudQuadContainerIndex, m_HookProtectionOffset, x, y);
+		x += 12;
+	}
 	if(pCharacter->m_EndlessJump)
 	{
 		HasCapabilities = true;
@@ -1675,6 +1698,12 @@ void CHud::RenderStatusIcons(int ClientId)
 		return;
 	}
 
+	if(g_Config.m_InfUseDDRaceHUD && GameClient()->m_GameInfo.m_HudDDRace)
+	{
+		// We render hook protection icon as a part of RenderPlayerState() if DDRace HUD is active
+		return;
+	}
+
 	const CGameClient::CClientData *pClientData = &GameClient()->m_aClients[ClientId];
 	int InfclassPlayerFlags = pClientData->m_InfClassPlayerFlags;
 
@@ -2106,7 +2135,7 @@ void CHud::OnRender()
 				RenderStatusIcons(GameClient()->m_aLocalIds[g_Config.m_ClDummy]);
 				RenderObjectOwnerIcons(GameClient()->m_aLocalIds[g_Config.m_ClDummy]);
 			}
-			if(GameClient()->m_Snap.m_aCharacters[GameClient()->m_Snap.m_LocalClientId].m_HasExtendedData && g_Config.m_ClShowhudDDRace && GameClient()->m_GameInfo.m_HudDDRace)
+			if(GameClient()->m_Snap.m_aCharacters[GameClient()->m_Snap.m_LocalClientId].m_HasExtendedData && GameClient()->m_GameInfo.m_HudDDRace)
 			{
 				RenderPlayerState(GameClient()->m_Snap.m_LocalClientId);
 			}
@@ -2129,7 +2158,6 @@ void CHud::OnRender()
 			}
 			if(SpectatorId != SPEC_FREEVIEW &&
 				GameClient()->m_Snap.m_aCharacters[SpectatorId].m_HasExtendedData &&
-				g_Config.m_ClShowhudDDRace &&
 				(!GameClient()->m_MultiViewActivated || GameClient()->m_MultiViewShowHud) &&
 				GameClient()->m_GameInfo.m_HudDDRace)
 			{

src/game/client/components/menus_settings.cpp
@@ -2689,6 +2689,7 @@ void CMenus::RenderSettingsAppearance(CUIRect MainView)
 		RightView.HSplitTop(MarginSmall, nullptr, &LeftView);
 
 		// Switches of various DDRace HUD elements
+		DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_InfUseDDRaceHUD, Localize("Use DDRace HUD for Infclass"), &g_Config.m_InfUseDDRaceHUD, &RightView, LineSize);
 		DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_InfcShowHookProtection, Localize("Show player hook protection"), &g_Config.m_InfcShowHookProtection, &RightView, LineSize);
 	}
 	else if(s_CurTab == APPEARANCE_TAB_CHAT)
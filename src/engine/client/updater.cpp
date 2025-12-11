infclass/infclass-client

Rewrite the updater for Infclass client

src/engine/client/updater.cpp
@@ -2,8 +2,10 @@
 
 #include "updater.h"
 #include <engine/client.h>
+#include <engine/client/infclass.h>
 #include <engine/engine.h>
 #include <engine/external/json-parser/json.h>
+#include <engine/shared/config.h>
 #include <engine/shared/http.h>
 #include <engine/shared/json.h>
 #include <engine/storage.h>
@@ -31,7 +33,7 @@ public:
 
 static const char *GetUpdaterUrl(char *pBuf, int BufSize, const char *pFile)
 {
-	str_format(pBuf, BufSize, "https://update.ddnet.org/%s", pFile);
+	str_format(pBuf, BufSize, "%s/%s", g_Config.m_ClInfcUpdatesUrl, pFile);
 	return pBuf;
 }
 

Pointer31/ddnet_client

src/engine/client/updater.cpp
@@ -5,7 +5,9 @@
 #include <engine/client.h>
 #include <engine/engine.h>
 #include <engine/external/json-parser/json.h>
+#include <engine/shared/config.h>
 #include <engine/shared/http.h>
+#include <engine/shared/infclass.h>
 #include <engine/shared/json.h>
 #include <engine/storage.h>
 
@@ -32,7 +34,7 @@ public:
 
 static const char *GetUpdaterUrl(char *pBuf, int BufSize, const char *pFile)
 {
-	str_format(pBuf, BufSize, "https://update.ddnet.org/%s", pFile);
+	str_format(pBuf, BufSize, "%s/%s", g_Config.m_ClInfcUpdatesUrl, pFile);
 	return pBuf;
 }
 
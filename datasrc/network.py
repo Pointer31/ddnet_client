06bd9a98a6a0ee7a985858e9cff4fc1862367f87
KebsCS <git@kebs.dev>
2026-01-23 21:40:07 +0100

    Add scoreboard popup with map info

--- datasrc/network.py
+++ datasrc/network.py
@@ -718,4 +718,8 @@ Messages = [
 	NetMessageEx("Cl_EnableSpectatorCount", "enable-spectator-count@netmsg.ddnet.org", [
 		NetBool("m_Enable"),
 	]),
+	
+	NetMessageEx("Sv_MapInfo", "map-info@netmsg.ddnet.org", [
+		NetString("m_pDescription"),
+	]),
 ]

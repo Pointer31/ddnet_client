infclass/infclass-client

Editor: Add initial PTUM support

src/game/editor/explanations.cpp
@@ -686,3 +686,8 @@ const char *CEditor::Explain(EExplanation Explanation, int Tile, int Layer)
 	dbg_assert(false, "logic error");
 	return nullptr;
 }
+
+const char *CEditor::Explain(const char *pZoneType, int Tile)
+{
+	return nullptr;
+}

Pointer31/ddnet_client

src/game/editor/explanations.cpp
@@ -756,3 +756,8 @@ const char *CExplanations::Explain(EGametype Gametype, int Tile, int Layer)
 	}
 	dbg_assert_failed("Gametype invalid: %d", (int)Gametype);
 }
+
+const char *CExplanations::Explain(const char *pZoneType, int Tile)
+{
+	return nullptr;
+}
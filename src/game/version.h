01cd5fa6252a43d4f0cdd912e2afe2c5a9fe64b6
Pointer31 <pointer.31.31.31@gmail.com>
2026-04-27 12:18:55 +0200

    set duckclient version to 5

--- src/game/version.h
+++ src/game/version.h
@@ -18,9 +18,8 @@ extern const char *GIT_SHORTREV_HASH;
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 #define GAME_NETVERSION7 "0.7 802f1be60a05665f"
 
-// duck client
-
-#define DUCKCLIENT_VERSIONNUM 4020
-#define DUCKCLIENT_VERSIONSTR "v4.2"
+// duck infclass client
+#define DUCKCLIENT_VERSIONNUM 5000
+#define DUCKCLIENT_VERSIONSTR "v5.0"
 
 #endif

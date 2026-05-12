d3a8ef9e170938a88c3fa9148a4652f0a8306726
Pointer31 <pointer.31.31.31@gmail.com>
2026-05-12 18:29:14 +0200

    add cl_main_menu_hide_server

--- src/engine/shared/config_variables.h
+++ src/engine/shared/config_variables.h
@@ -767,6 +767,7 @@ MACRO_CONFIG_INT(ClMapChangeMessage, cl_map_change_message, 0, 0, 2, CFGFLAG_CLI
 MACRO_CONFIG_STR(ClGametypeFilterList, cl_gametype_filter_list, 128, "", CFGFLAG_CLIENT | CFGFLAG_SAVE, "Gametype buttons filter list")
 MACRO_CONFIG_STR(ClSendLanguage, cl_send_language, 128, "", CFGFLAG_CLIENT | CFGFLAG_SAVE, "Language code to send to servers telling them to use this language")
 MACRO_CONFIG_INT(ClBrowserButtonPosition, cl_browser_button_position, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Where to place the browser button in the in-game UI")
+MACRO_CONFIG_INT(ClMainMenuHideServer, cl_main_menu_hide_server, 1, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Whether to hide the start server button on the main menu")
 
 // Client identify (from Kaizo Network)
 MACRO_CONFIG_INT(ClSendClientType, cl_send_client_type, 1, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Let know other clients that we are DuckClient")
